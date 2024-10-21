6cf1dadd19301b838db83ff453005873d66b6fb2
Aditi <aditidarade1@gmail.com>
2024-10-21 13:44:55 -0500

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Lifetime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public string MaskCreditCard(string input)
+        {
+            string[] parts = input.Split(' ');
+            string MaskedCC = "";
+            string output = "";
+            string seperator = " ";
+            bool CCFound = false;
+            foreach (string part in parts)
+            {
+                if (part.Length == 19 && part[4] == '-' && part[9] == '-' && part[14] == '-' && !(part.Contains(' ')))
+                {
+                    CCFound = true;
+                    MaskedCC = "XXXX-XXXX-XXXX-" + part[15] + "" + part[16] + "" + part[17] + "" + part[18];
+                    output = output + MaskedCC + seperator;
+                }
+                else
+                {
+                    output = output + part + seperator;
+                }
+
+            }
+            if (CCFound == false)
+            {
+                output = "Please enter your correct credit card number in the format XXXX-XXXX-XXXX-XXXX with no blank spaces.";
+            }
+            return output;
+        }
+        public string MaskSocialSecurity(string input)
+        {
+            string[] parts = input.Split(' ');
+            string separator = " ";
+            string MaskInput = "";
+            bool SSNFound = false;
+
+            foreach (string part in parts)
+            {
+                if (part.Length == 11 && part[3] == '-' && part[6] == '-' && !(part.Contains(' ')))
+                {
+                    SSNFound = true;
+                    //Console.Write("XXX-" + part[4] + "" + part[5] + "-XXXX");
+                    MaskInput = MaskInput + "XXX-" + part[4] + "" + part[5] + "-XXXX ";
+                }
+                else
+                {
+                    MaskInput = MaskInput + part + separator;
+                    //Console.Write(part + separator);
+                }
+            }
+            //Console.WriteLine("");
+            if (!SSNFound)
+            {
+                MaskInput = "Please enter your correct social security number in the format XXX-XX-XXXX with no blank spaces.";
+            }
+            return MaskInput;
+        }
+        public string MaskData(string input)
+        {
+            string MaskCCOutput = MaskCreditCard(input);
+            string MaskCCnSSNOutput = MaskSocialSecurity(MaskCCOutput);
+
+            return MaskCCnSSNOutput;
+        }
+    }
+}
---
+++ Sec26_Assign56_DataMasking/Program.cs
@@ -0,0 +1,37 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sec26_Assign56_DataMasking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            Console.WriteLine(class1.MaskCreditCard("My credit card number is 1234-5678-9012-3456"));
+            Console.WriteLine("-------------------------------------------------------------------------------");
+            Console.WriteLine(class1.MaskCreditCard("1234-5678-9012-345"));
+            Console.WriteLine("-------------------------------------------------------------------------------");
+            Console.WriteLine(class1.MaskCreditCard("My credit card number is 1234-567 -9012-3456"));
+            Console.WriteLine("-------------------------------------------------------------------------------");
+
+            Console.WriteLine(class1.MaskSocialSecurity("My social security number is 123-47-6 89"));
+            Console.WriteLine("-------------------------------------------------------------------------------");
+            Console.WriteLine(class1.MaskSocialSecurity("My social security number is 123-47-6789"));
+            Console.WriteLine("-------------------------------------------------------------------------------");
+
+            Console.WriteLine(class1.MaskData("My credit card number is 1234-5678-9012-3456 and my social security number is 123-47-6789"));
+            Console.WriteLine("-------------------------------------------------------------------------------");
+            Console.WriteLine(class1.MaskData("My credit card number is 1234-5678-9012-3456 and my social security number is 1 3-47-6789"));
+            Console.WriteLine("-------------------------------------------------------------------------------");
+            Console.WriteLine("-------------------------------------------------------------------------------");
+            Console.WriteLine(class1.MaskData("My credit card number is 1234-5678-9012-3456 and my social security number is 123-47-6789"));
+
+            Console.ReadKey();
+        }
+    }
+}
